e591f8642464b51142c9745a1823d42d5bf3f6c6
Vinicius Cãndido <viniciuscandidof320@gmail.com>
2026-05-09 22:23:19 -0300

    estudo de class

--- codes/POO/exercise/ex003/ex003.py
+++ codes/POO/exercise/ex003/ex003.py
@@ -2,12 +2,31 @@ class ContaBancaria:
     """
 cria uma conta bancária e permite fazer saques e depósitoa
     """
-    def __init__(self, id, name, saldo = 0):
+    def __init__(self, id, name,saldo= 0):
         self.id = id
         self.titular = name
         self.saldo = saldo
+        print(f"Conta {self.id} criada com sucesso. Saldo atual de R${self.saldo:,.2f}")
+
+    def __str__(self):
+        return f"A conta {self.id} de {self.titular} tem R${self.saldo:.2f} de saldo."
+
+    def depositar(self, valor):
+        self.saldo += valor
+        print(f"depósito de R${valor:,.2f} autorizado na conta {self.id}")
+
+    def sacar(self, valor):
+        if valor > self.saldo:
+           print(f'Saque NEGADO de R${valor:,.2f} na conta {self.id}: SALDO INSUFICIENTE')
+        else:
+            self.saldo -= valor
+            print(f'Saque de R%{valor:,.2f} autorizado na conta {self.id} ')
 
 
 
 
-c1 = ContaBancaria(122, vini, 3000)
+
+c1 = ContaBancaria(122,"vini",3000 )
+c1.depositar(500)
+c1.sacar(20000)
+print(c1)
