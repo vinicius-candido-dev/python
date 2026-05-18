327a43ea6bba66540afd172db5d9fbc14af2ff07
Vinicius Cãndido <viniciuscandidof320@gmail.com>
2026-05-18 15:53:59 -0300

    Update ex001.py

--- codes/POO/OOP 2/ex001.py
+++ codes/POO/OOP 2/ex001.py
@@ -6,7 +6,11 @@ class Empty:
         self.pay = pay
         self.email = first + '.' + last + '@company.com'
 
+    def fullname(self):
+        return f'{self.first} {self.last}'
+
 
 pessoa = Empty('vini','maia',5000)
 print(pessoa.email)
 print(pessoa.pay)
+print(pessoa.fullname())
